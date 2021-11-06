a1c53d37bb680cde2567bafaf25fa776517eb36c
NBakalik <bakalik.nazariy@gmail.com>
2021-11-06 14:15:34 +0200

    implemented Google maps

--- app/src/main/java/com/example/testgooglemapsapi/MainActivity.java
+++ app/src/main/java/com/example/testgooglemapsapi/MainActivity.java
@@ -1,5 +1,26 @@
 package com.example.testgooglemapsapi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-public class MainActivity extends AppCompatActivity {
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        supportMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+    }
 }
